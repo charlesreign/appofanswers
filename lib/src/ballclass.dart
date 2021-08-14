bc8411d68dc0602d00425d396523234be3d0681b
Charles Reign <charlesgold45@gmail.com>
2021-08-14 15:47:08 +0100

    added image asset and work on card widget

--- lib/src/ballclass.dart
+++ lib/src/ballclass.dart
@@ -1,17 +1,46 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class BallClass extends StatefulWidget {
-
-
   @override
   _BallClassState createState() => _BallClassState();
 }
 
 class _BallClassState extends State<BallClass> {
+  int num = 1;
+
+  void generate() {
+    num = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'magic ball app',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0,
+            //fontFamily: 'Vesta Night - Personal Use',
+          ),
+        ),
+        SizedBox(
+          height: 25.0,
+        ),
+        Card(
+          margin: EdgeInsets.all(10.0),
+          child: Container(
+            child: TextButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Image.asset('images/ball1.png'),
+                )),
+          ),
+        )
+      ],
     );
   }
 }
