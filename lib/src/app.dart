1cd1cc9a618f394e255ab7065194ccdb049c9c7e
Charles Reign <charlesgold45@gmail.com>
2021-08-14 15:04:05 +0100

    initialize project

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('magic 8 ball'),
+        ),
+      ),
+    );
+  }
+}
